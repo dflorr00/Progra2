4ff7ff9230f9f5a66a9e56164e752b531587c52b
daniflo <daniflo@Portatil_Dani>
2020-12-20 15:23:20 +0100

    Tablero: Modificado el comentario del constructor.

--- Trenes/src/Codigo/Tablero.java
+++ Trenes/src/Codigo/Tablero.java
@@ -7,7 +7,7 @@ public class Tablero {
 	private String fila2 = "   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9\n";
 	private Casilla[][] tablero;
 	
-	//Constructor
+	//Constructor con casillas vacías
 	public Tablero() {
 		tablero=new Casilla[tam][tam];
 		for(int i=0;i<tam;i++) {
